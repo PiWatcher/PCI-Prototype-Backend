0ffaeb6e03fd6d8122da93ba993cf2641e5b95f9
cforonda <champ.foronda@gmail.com>
2020-11-17 14:28:33 -0700

    Updated template data for endpoint resource

--- backend/resources/ApiResource.py
+++ backend/resources/ApiResource.py
@@ -2,7 +2,7 @@ import random
 import datetime
 
 from backend import app
-from backend.services import MongoService, RandomDataService
+from backend.services import MongoService
 from flask_restful import Resource, Api
 
 class ApiBaseResource(Resource):
@@ -16,12 +16,18 @@ class ApiBuidlingResource(Resource):
 class ApiEndpointResource(Resource):
     def get(self, building, endpoint):
 
+        building_map = {'90': 'SICCS'}
+
+        room_map = {'100': 'Main Lobby'}
+
         random_count = random.randint(1, 10)
         current_time = datetime.datetime.now()
 
         return {'data': {'timestamp': f'{current_time}',
-                         'building': f'building',
-                         'endpoint': f'endpoint',
+                         'building_id': f'{building}',
+                         'building_name': f'{building_map[building]}',
+                         'endpoint_id': f'{endpoint}',
+                         'endpoint_name': f'{room_map[endpoint]}',
                          'count': random_count}}
     
     def post(self, building, endpoint):
