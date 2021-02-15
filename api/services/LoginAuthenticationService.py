7b7ae1bf2d36ad454c3c393cfabe0b5ba5e9fbd7
Champ Foronda <champ.foronda@gmail.com>
2021-02-15 10:37:24 -0700

    Added stubbed functions for Login Authentication Service

--- api/services/LoginAuthenticationService.py
+++ api/services/LoginAuthenticationService.py
@@ -1,10 +1,24 @@
+import api
+
 class LoginAuthenticationService():
 
     def __init__(self):
-        pass
+        self.mongo = api.mongo
 
     def handle_signin(self, data):
         pass
 
     def handle_signup(self, data):
+        pass
+
+    def signin(self, data):
+        pass
+
+    def signup(self, data):
+        pass
+
+    def user_exists(self, data):
+        pass
+
+    def verify_password(self, password):
         pass
