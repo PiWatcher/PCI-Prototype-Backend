874ca2f7d17ed56859dcfbfec8378aaf18491acb
Champ Foronda <champ.foronda@gmail.com>
2021-02-21 13:24:40 -0700

    Updated development config and base configuration

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,7 @@
 class BaseConfig():
     API_PREFIX = "/api"
+    BASE_URL = "0.0.0.0"
+    PORT = 5000
     TESTING = False
     DEBUG = False
 
@@ -8,7 +10,9 @@ class DevelopmentConfig(BaseConfig):
     DEBUG = True
     MONGO_USER = ""
     MONGO_PASS = ""
-    MONGODB_URI = f"mongodb+srv://{MONGO_USER}:{MONGO_PASS}@cluster0.vdjw7.mongodb.net/"
+    MONGO_HOSTNAME = "mongodb"
+    MONGO_PORT = "27017"
+    MONGODB_URI = f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOSTNAME}:{MONGO_PORT}/"
 
 class ProductionConfig(BaseConfig):
     FLASK_ENV = "production"
