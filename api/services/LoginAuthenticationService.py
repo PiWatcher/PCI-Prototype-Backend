9a8894b89ca7a4a014b56c22c5cba89969c02057
Champ Foronda <champ.foronda@gmail.com>
2021-04-27 00:44:10 -0700

    Documented LoginAuthenticationService

--- api/services/LoginAuthenticationService.py
+++ api/services/LoginAuthenticationService.py
@@ -9,36 +9,101 @@ import datetime
 class LoginAuthenticationService(BaseService):
 
     def handle_creating_roles(self, data):
+        '''
+        A public handler function for creating roles
+
+        @returns a response object for creating a role
+        '''
+
         return self.__create_role(data)
 
     def handle_deleting_role(self, data):
+        '''
+        A public handler function for deleting roles
+
+        @returns a response object for deleting a role 
+        '''
+
         return self.__delete_role(data)
 
     def handle_deleting_user(self, data):
+        '''
+        A public handler function for deleting a user
+
+        @returns a response object for deleting a role
+        '''
+
         return self.__delete_user(data)
 
     def handle_grabbing_users(self):
+        '''
+        A public handler function for grabbing users
+
+        @returns a response object for grabbing users
+        '''
+
         return self.__grab_users()
 
     def handle_grabbing_roles(self):
+        '''
+        A public handler function for grabbing roles
+
+        @returns a response object for grabbing roles
+        '''
+
         return self.__grab_roles()
 
     def handle_signin(self, data):
+        '''
+        A public handler function for signing to an account
+
+        @returns a response object for the signed in user
+        '''
+
         return self.__signin(data)
 
     def handle_token_signin(self, data):
+        '''
+        A public handler function for signing with a user token
+
+        @returns a response object for the signed in user
+        '''
+
         return self.__token_signin(data)
 
     def handle_signup(self, data):
+        '''
+        A public handler function for signing up for a user account
+
+        @returns a response object for a signed up account
+        '''
         return self.__signup(data) 
 
     def handle_updating_user_role(self, data):
+        '''
+        A public handler function for updating a user's role
+
+        @returns a response object for the updated user
+        '''
+
         return self.__update_user_role(data)
 
     def handle_updating_password(self, email, data):
+        '''
+        A public handler function for updating a user's password
+
+        @returns a response object for the updated user
+        '''
+
         return self.__update_user_password(email, data)
 
     def validate_permission(self, email):
+        '''
+        Validates a users permission to access the admin dashboard
+
+        @return a boolean value
+        '''
+
         potential_user = self.__grab_user(email)
         user_role = self.__grab_role(potential_user["role"])
 
@@ -48,6 +113,14 @@ class LoginAuthenticationService(BaseService):
         return False
 
     def __create_role(self, data):
+        '''
+        Creates a new role in the database
+
+        @raises RoleAlreadyExistsError if that role already exists
+
+        @return a response object
+        '''
+
         try:
             # check if role already exists
             if self.__grab_role(data['role_name']) is not None:
@@ -81,6 +154,15 @@ class LoginAuthenticationService(BaseService):
             return super().construct_response(error_message)
 
     def __delete_role(self, data):
+        '''
+        Deletes a role from the database
+
+        @raises SchemaValidationError if the schema does not contain the correct keys
+        @raises RoleDoesNotExistError if the role does not exist
+        
+        @returns a response object
+        '''
+
         try:
             # grab roles from data
             role_name = data.get("role_name", None)
@@ -121,6 +203,15 @@ class LoginAuthenticationService(BaseService):
             return super().construct_response(error_message)
 
     def __delete_user(self, data):
+        '''
+        Deletes a user from the database
+
+        @raises SchemaValidationError if the schema is missing a required key
+        @raises EmailDoesNotExistError if the email does not exist in the database
+
+        @returns a response object
+        '''
+
         try:
             # grab email from data
             email = data.get("email", None)
@@ -161,6 +252,14 @@ class LoginAuthenticationService(BaseService):
             return super().construct_response(error_message)
 
     def __grab_user(self, email):
+        '''
+        Grabs a single user from the mongodb database
+
+        @param email used to query for a single user
+
+        @returns the potential user that is grabbed from the database
+        '''
+
         # find user in the database by email
         potential_user = super().get_database('Users')['users'].find_one({
             "email": email
@@ -174,6 +273,11 @@ class LoginAuthenticationService(BaseService):
         return None 
 
     def __grab_users(self):
+        '''
+        Grabs all the users from the mongodb database
+
+        @returns a response object
+        '''
         try:
             # grab all user accounts from the database
             user_entries = super().get_database("Users")["users"].find({}, {
@@ -198,12 +302,25 @@ class LoginAuthenticationService(BaseService):
             return super().construct_response(error_message)
 
     def __grab_role(self, role_name):
+        '''
+        Grabs a role by role name from the database
+
+        @param role_name the role to be grabbed from the database
+
+        @return the role from the database
+        '''
         # attempt to grab role from database and return it
         return super().get_database("Users")["roles"].find_one({
             'role_name': role_name
             }, {"_id": 0})
 
     def __grab_roles(self):
+        '''
+        Grabs all roles that are defined in the database
+
+        @returns a response object
+        '''
+
         try:
             # grab all entries from the roles collections
             roles_entries = super().get_database("Users")["roles"].find({}, {"_id": 0})
@@ -223,6 +340,17 @@ class LoginAuthenticationService(BaseService):
             return super().construct_response(error_message)
 
     def __signin(self, data):
+        '''
+        Functionality for signing into an account
+
+        @param data a json object that contains the email and password
+                    for an account
+
+        @raises SchemaValidationError if it is missing required fields
+
+        @returns a response object
+        '''
+
         try:
             email = data.get("email", None)
             password = data.get("password", None)
@@ -265,6 +393,17 @@ class LoginAuthenticationService(BaseService):
             return super().construct_response(error_message)
 
     def __signup(self, data):
+        '''
+        Functionality for creating a user account in the database
+
+        @param data json object the contains the required fields
+
+        @raises SchemaValidationError if it is missing required fields
+        @raises EmailAlreadyExistsError if an account already has that email
+
+        @returns a response object
+        '''
+
         try:
             email = data.get("email", None)
             password = data.get("password", None)
@@ -315,7 +454,17 @@ class LoginAuthenticationService(BaseService):
             return super().construct_response(error_message)
         
     def __token_signin(self, data):
-        try:        
+        '''
+        Functionality for signing in with a user/jwt token
+
+        @param data the email used to grab a user
+
+        @raises EmailDoesNotExistError if the email does not exist
+
+        @returns a response object
+        '''
+
+        try: 
             user = self.__grab_user(data)
 
             if user is None:
@@ -337,6 +486,17 @@ class LoginAuthenticationService(BaseService):
             return super().construct_response(error_message)
 
     def __update_user_role(self, data):
+        '''
+        Updates a users role in the database
+
+        @param data contains the required keys for updating a user role
+
+        @raises SchemaValidationError if the data is missing required fields
+        @raises RoleDoesNotExistError if the role does not exist in the database
+
+        @returns a response object
+        '''
+
         try:
             email = data.get('email', None)
             new_role = data.get('new_role', None)
@@ -394,6 +554,17 @@ class LoginAuthenticationService(BaseService):
             return super().construct_response(errors["InternalServerError"])
 
     def __update_user_password(self, email, data):
+        '''
+        Updates a user's password in the database
+
+        @param data the required keys for updating the user password
+
+        @raises SchemaValidationError if the data is missing required keys
+        @raises UnauthorizedError if the account failed to authorize
+
+        @returns a response object
+        '''
+
         try:
             # validate schema of data
             password = data.get("password", None)
@@ -450,6 +621,14 @@ class LoginAuthenticationService(BaseService):
             return super().construct_response(error_message)
         
     def __verify_login(self, email, password):
+        '''
+        Verify a users login by validating the user with an email/password
+
+        @param email the key to search the user by
+        @param password the password to check upon
+
+        @returns a boolean value
+        '''
 
         potential_user = self.__grab_user(email)
 
