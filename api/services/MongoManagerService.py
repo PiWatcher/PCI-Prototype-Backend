2a1ea62d138469744764fc1c015d96184492833a
Champ Foronda <champ.foronda@gmail.com>
2021-04-27 22:25:44 -0700

    Changed delta time to be 8 hours

--- api/services/MongoManagerService.py
+++ api/services/MongoManagerService.py
@@ -292,7 +292,7 @@ class MongoManagerService(BaseService):
 
             # construct time interval
             current_time = datetime.now()
-            interval = current_time - timedelta(hours=6)
+            interval = current_time - timedelta(hours=8)
 
             # construct query filter
             query_filter = {
